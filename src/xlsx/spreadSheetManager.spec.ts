dae71c21101d087b8398832f046475a3776e863d
Bostjan Kamnik <b.kamnik@sportradar.com>
2023-03-12 03:30:03 +0100

    add readInSpreadsheet tests and implementation

--- src/xlsx/spreadSheetManager.spec.ts
+++ src/xlsx/spreadSheetManager.spec.ts
@@ -3,21 +3,44 @@ import {
   readInSpreadsheet,
 } from "./spreadSheetManager";
 import fs from "fs";
+import Game from "../models/game";
 
-describe("generateStarterSpreadsheet", () => {
-  it('should create a file name "test.xlsx', () => {
-    const starterFileName = "test";
-    generateStarterSpreadsheet(starterFileName);
-    const filePath = `./${starterFileName}.xlsx`;
-    
-    expect(fs.existsSync(filePath)).toBeTruthy();
+describe("spreadSheetManager", () => {
+  const starterFileName = "test";
+  const starterFilePath = `./${starterFileName}.xlsx`;
+
+  describe("generateStarterSpreadsheet", () => {
+    it('should create a file name "test.xlsx', () => {
+      generateStarterSpreadsheet(starterFileName);
+
+      expect(fs.existsSync(starterFilePath)).toBeTruthy();
+    });
+
+    it("should create a file given a directory path + name", () => {
+      const fileNameSubdir = "./output/test";
+      generateStarterSpreadsheet(fileNameSubdir);
+      const filePath = `./${fileNameSubdir}.xlsx`;
+
+      expect(fs.existsSync(filePath)).toBeTruthy();
+    });
   });
 
-  it("should create a file given a directory path + name", () => {
-    const fileNameSubdir = "./output/test";
-    generateStarterSpreadsheet(fileNameSubdir);
-    const filePath = `./${fileNameSubdir}.xlsx`;
-    
-    expect(fs.existsSync(filePath)).toBeTruthy();
+  describe("readInSpreadsheet", () => {
+    let gamesArray: Game[];
+
+    beforeEach(() => {
+      gamesArray = readInSpreadsheet(starterFilePath);
+    });
+
+    it("should return an array with a single entry", () => {
+      expect(gamesArray).toBeDefined();
+      expect(gamesArray.length).toBe(1);
+    });
+
+    it("should return an object of type Game", () => {
+      const game = gamesArray[0];
+      expect(game).toMatchObject<Game>({ id: "0", boardGameGeekId: "0" });
+      expect(game instanceof Game).toBeTruthy();
+    });
   });
 });
--- src/xlsx/spreadSheetManager.ts
+++ src/xlsx/spreadSheetManager.ts
@@ -6,7 +6,7 @@ import path from "path";
 function _stripExtension(filePath: string) {}
 
 function generateStarterSpreadsheet(filePath: string) {
-  const rows = [new Game("0", "12932")];
+  const rows = [new Game("0", "0")];
   const worksheet = XLSX.utils.json_to_sheet(rows);
   const workbook = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(workbook, worksheet, "Games");
@@ -21,9 +21,22 @@ function generateStarterSpreadsheet(filePath: string) {
   XLSX.writeFile(workbook, outFilePath);
 }
 
-function readInSpreadsheet(fileName: string): Game[] {
-  const workbook = XLSX.readFile(fileName);
-  return XLSX.utils.sheet_to_json(workbook.Sheets["Games"]);
+function _parseGame(gameProps: any) {
+  if (!gameProps.id || !gameProps.boardGameGeekId) {
+    // todo: log error
+    return undefined;
+  }
+
+  const game = new Game(gameProps.id, gameProps.boardGameGeekId);
+  return Object.assign(game, gameProps);
+}
+
+function readInSpreadsheet(filePath: string): Game[] {
+  const workbook = XLSX.readFile(filePath);
+  const gamesPlain = XLSX.utils.sheet_to_json(workbook.Sheets["Games"]);
+  return gamesPlain
+    .map((gameProps) => _parseGame(gameProps))
+    .filter((game) => game); // remove undefined
 }
 
 export { generateStarterSpreadsheet, readInSpreadsheet };
