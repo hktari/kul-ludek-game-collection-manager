32bf26d150287b1843beac3f3d1681082899b280
hktari <bkamnik1995@gmail.com>
2023-03-15 06:06:22 +0100

    add types and change return type to include errors

--- src/dom/websiteInspector.ts
+++ src/dom/websiteInspector.ts
@@ -16,6 +16,9 @@ export type PropertyCSSSelector = {
  */
 export type SelectedWebData = Record<string, string | undefined>
 
+export type SelectionErrors = Record<string, string>
+
+export type QueryResult = [SelectedWebData, SelectionErrors]
 /**
  * Uses browser automation to open up a website and perform data queries with css
  */
@@ -24,6 +27,11 @@ export default class WebsiteInspector {
 
   constructor() { }
 
+
+  private _mapToObjectOrUndefined(map: Map<any, any>) {
+    return map.size === 0 ? undefined : Object.fromEntries(map)
+
+  }
   /**
    * 
    * @param url the website URL
@@ -33,7 +41,7 @@ export default class WebsiteInspector {
   async performQueries(
     url: URL,
     ...selectors: PropertyCSSSelector[]
-  ): Promise<SelectedWebData> {
+  ): Promise<QueryResult> {
     try {
       if (!this.browser) {
         this.browser = await puppeteer.launch({ headless: true });
@@ -45,28 +53,31 @@ export default class WebsiteInspector {
 
       const selectedData = new Map();
 
+      const errors = new Map()
+
       for (const textSelector of selectors) {
         for (const [key, selector] of Object.entries(textSelector)) {
           try {
+            // todo: returns an array
             let elementText = await page.$eval(
               selector,
               (elem) => (elem as HTMLElement).innerText
-            );
-
+            )
             elementText = elementText.trim();
 
             selectedData.set(key, elementText);
-          } catch (error) {
+          } catch (error: any) {
             console.error(
               `Failed to get element innerText. Selector: ${selector}`,
               error
             );
             selectedData.set(key, undefined);
+            errors.set(key, error.message)
           }
         }
       }
 
-      return Object.fromEntries(selectedData);
+      return [Object.fromEntries(selectedData), this._mapToObjectOrUndefined(errors)];
     } catch (err) {
       console.error(err);
       throw err;
