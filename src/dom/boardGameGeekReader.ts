16a1c4068df184b6cec8043a781aec7552c44d93
hktari <bkamnik1995@gmail.com>
2023-03-15 05:00:58 +0100

    specify selectors

--- src/dom/boardGameGeekReader.ts
+++ src/dom/boardGameGeekReader.ts
@@ -4,6 +4,7 @@ import Game from "../models/game";
 import { Parser } from "htmlparser2";
 import { DomHandler } from "htmlparser2";
 import { DomUtils } from "htmlparser2";
+import WebsiteInspector, { PropertyCSSSelector, SelectedWebData } from "./websiteInspector";
 
 const CSSselect = require("css-select");
 
@@ -13,37 +14,42 @@ export type BoardGameGeekResource = Partial<Game> & {
 };
 
 export default class BoardGameGeekReader {
-  constructor(public baseUrl: string) {}
+  private websiteInspector: WebsiteInspector;
+
+  constructor(public baseUrl: string) {
+    this.websiteInspector = new WebsiteInspector()
+  }
+
+
+  private _getSelectorsForGame(): PropertyCSSSelector[] {
+    // todo: read from config file
+    return [
+      { title: 'div.game-header-title:nth-child(2) > div:nth-child(2) > h1:nth-child(1) > a:nth-child(1)' },
+      { description: '.game-description-body > div:nth-child(1)' },
+      { minAge: 'li.gameplay-item:nth-child(3) > div:nth-child(1) > span:nth-child(1)' },
+      //{ maxAge:             },
+      { minMaxPlayers: 'li.gameplay-item:nth-child(1) > div:nth-child(1) > span:nth-child(1)' },
+      { categories: 'li.feature:nth-child(2) > div:nth-child(3) > popup-list:nth-child(1) > span > a' },
+      { minMaxPlaytime: 'li.gameplay-item:nth-child(2) > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)' },
+      //v developer:            string},
+      { publisher: 'div.game-header-credits:nth-child(3) > ng-include:nth-child(1) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(3) > popup-list:nth-child(2) > span:nth-child(2) > a:nth-child(1)' },
+      { releaseDateYear: '.game-year' },
+      //{ errors?:             strin}g
+    ]
+  }
+
+  private _parseDataIntoGame(websiteData: SelectedWebData){
+
+  }
+  
 
   getResourceForGameId(gameId: string) {
     return new Promise<BoardGameGeekResource>(async (resolve, reject) => {
       const resourceUri = `/boardgame/${gameId}`;
       const gameUrl = new URL(resourceUri, this.baseUrl);
-      const html = await getContentAtUrl(gameUrl);
 
-      const handler = new DomHandler((error, dom) => {
-        if (error) {
-          // Handle error
-          reject(error);
-        } else {
-          // Parsing completed, do something
-          console.log(dom);
+      const websiteData = await this.websiteInspector.performQueries(gameUrl, ...this._getSelectorsForGame())
 
-          let boardGGResource: BoardGameGeekResource = {
-            boardGameGeekId: gameId,
-            title: CSSselect.selectOne(
-              ".game-header-title:nth-child(2) > div:nth-child(2) > h1:nth-child(1) > a",
-              dom
-            ),
-            timestamp: new Date().toISOString(),
-          };
-
-          resolve(boardGGResource);
-        }
-      });
-      const parser = new Parser(handler);
-      parser.write(html);
-      parser.end();
     });
   }
 }
