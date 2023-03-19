6276b6515084a183581464ae081c5edee7411573
Bostjan Kamnik <b.kamnik@sportradar.com>
2023-03-19 18:57:42 +0100

    add commander and 'update' command

--- TODO.md
+++ TODO.md
@@ -4,5 +4,6 @@
 - parse data into an array of models [OK]
 - get DOM for given boardgamegeek id [OK]
 - get value for given boardgamegeek DOM selector [OK]
+- add main programme interface (command line arguments)
 - edit and save table file
 - log errors occured during processing an entry into the 'errors' column
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import { generateStarterSpreadsheet } from "./xlsx/spreadSheetManager";
+import { Command } from "commander";
+const program = new Command();
 
-console.log("Start");
+program
+  .name("gcm")
+  .description(
+    `Game Collection Manager manages a spreadsheet file. Updating it with data from board game websites. The resulting spreadsheet can be used to search and manage your board game collection`
+  )
+  .version("0.1.0");
 
-// generateStarterSpreadsheet();
+program
+  .command("update")
+  .description(
+    "Updates a spreadsheet file with newest data pulled from the web."
+  )
+  .argument("<input-file>", "Your board game collection in a .xlsx file")
+  .action((inputFile, options) => {
+    console.log(`Parsing file ${inputFile}...`);
 
-console.log("End");
+    console.log('Done.')
+  });
+
+program.parse();
 
 export default {};
