9b3f1ff2928ad98cbdcaf9ffc4d0c8f523e90430
Boštjan Kamnik <bkamnik1995@gmail.com>
2023-04-12 10:11:13 +0200

    call resolve

--- src/dom/boardGameGeekReader.ts
+++ src/dom/boardGameGeekReader.ts
@@ -4,53 +4,70 @@ import Game from "../models/game";
 import { Parser } from "htmlparser2";
 import { DomHandler } from "htmlparser2";
 import { DomUtils } from "htmlparser2";
-import WebsiteInspector, { PropertyCSSSelector, SelectedWebData } from "./websiteInspector";
+import WebsiteInspector, {
+  PropertyCSSSelector,
+  SelectedWebData,
+} from "./websiteInspector";
 import { parseMinMaxPlayers } from "./boardGameGeekReader.util";
 import { off } from "process";
 
 const CSSselect = require("css-select");
 
-
 export type BoardGameGeekResource = Partial<Game> & {
   timestamp: string;
 };
-
 
 export default class BoardGameGeekReader {
   private websiteInspector: WebsiteInspector;
 
   constructor(public baseUrl: string) {
-    this.websiteInspector = new WebsiteInspector()
+    this.websiteInspector = new WebsiteInspector();
   }
-
 
   private _getSelectorsForGame(): PropertyCSSSelector[] {
     // todo: read from config file
     return [
-      { title: 'div.game-header-title:nth-child(2) > div:nth-child(2) > h1:nth-child(1) > a:nth-child(1)' },
-      { description: '.game-description-body > div:nth-child(1)' },
-      { minAge: 'li.gameplay-item:nth-child(3) > div:nth-child(1) > span:nth-child(1)' },
+      {
+        title:
+          "div.game-header-title:nth-child(2) > div:nth-child(2) > h1:nth-child(1) > a:nth-child(1)",
+      },
+      { description: ".game-description-body > div:nth-child(1)" },
+      {
+        minAge:
+          "li.gameplay-item:nth-child(3) > div:nth-child(1) > span:nth-child(1)",
+      },
       //{ maxAge:             },
-      { minMaxPlayers: 'li.gameplay-item:nth-child(1) > div:nth-child(1) > span:nth-child(1)' },
-      { categories: 'li.feature:nth-child(2) > div:nth-child(3) > popup-list:nth-child(1) > span > a' },
-      { minMaxPlaytime: 'li.gameplay-item:nth-child(2) > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)' },
+      {
+        minMaxPlayers:
+          "li.gameplay-item:nth-child(1) > div:nth-child(1) > span:nth-child(1)",
+      },
+      {
+        categories:
+          "li.feature:nth-child(2) > div:nth-child(3) > popup-list:nth-child(1) > span > a",
+      },
+      {
+        minMaxPlaytime:
+          "li.gameplay-item:nth-child(2) > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)",
+      },
       //v developer:            string},
-      { publisher: 'div.game-header-credits:nth-child(3) > ng-include:nth-child(1) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(3) > popup-list:nth-child(2) > span:nth-child(2) > a:nth-child(1)' },
-      { releaseDateYear: '.game-year' },
+      {
+        publisher:
+          "div.game-header-credits:nth-child(3) > ng-include:nth-child(1) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(3) > popup-list:nth-child(2) > span:nth-child(2) > a:nth-child(1)",
+      },
+      { releaseDateYear: ".game-year" },
       //{ errors?:             strin}g
-    ]
+    ];
   }
-
 
   private _parseDataIntoGame(websiteData: SelectedWebData) {
     let minPlayers, maxPlayers;
     if (websiteData.minMaxPlayers) {
-      [minPlayers, maxPlayers] = parseMinMaxPlayers(websiteData.minMaxPlayers)
+      [minPlayers, maxPlayers] = parseMinMaxPlayers(websiteData.minMaxPlayers);
     }
 
     let minAge;
     if (websiteData.minAge) {
-      minAge = +websiteData.minAge
+      minAge = +websiteData.minAge;
     }
 
     const game: Partial<BoardGameGeekResource> = {
@@ -62,25 +79,32 @@ export default class BoardGameGeekReader {
       genre: websiteData.categories,
       publisher: websiteData.publisher,
       releaseDateYear: websiteData.releaseDateYear,
-    }
+    };
 
     return game;
   }
-
 
   getResourceForGameId(gameId: string) {
     return new Promise<BoardGameGeekResource>(async (resolve, reject) => {
       const resourceUri = `/boardgame/${gameId}`;
       const gameUrl = new URL(resourceUri, this.baseUrl);
 
-      const [websiteData, errors] = await this.websiteInspector.performQueries(gameUrl, ...this._getSelectorsForGame())
+      const [websiteData, errors] = await this.websiteInspector.performQueries(
+        gameUrl,
+        ...this._getSelectorsForGame()
+      );
 
-      const gameResource = this._parseDataIntoGame(websiteData)
+      const gameResource: BoardGameGeekResource = {
+        timestamp: new Date().toISOString(),
+        errors:
+          errors &&
+          Object.entries(errors)
+            .map((attr, err) => `${[attr]}: ${err}`)
+            .join("\n"),
+        ...this._parseDataIntoGame(websiteData),
+      };
 
-      gameResource.timestamp = new Date().toISOString()
-      gameResource.errors = errors && Object.entries(errors).map((attr, err) => `${[attr]}: ${err}`).join('\n')
-      
-      return gameResource
+      return resolve(gameResource);
     });
   }
 }
