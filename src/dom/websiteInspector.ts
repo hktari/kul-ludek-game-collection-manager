f19fd048f2b63a816024cdfdf5805286f913e787
Boštjan Kamnik <bkamnik1995@gmail.com>
2023-04-12 10:11:13 +0200

    run linter

--- src/dom/websiteInspector.ts
+++ src/dom/websiteInspector.ts
@@ -14,26 +14,24 @@ export type PropertyCSSSelector = {
  * Key is given property name
  * Value is the text content of the DOM Element selected or undefined if none was found
  */
-export type SelectedWebData = Record<string, string | undefined>
+export type SelectedWebData = Record<string, string | undefined>;
 
-export type SelectionErrors = Record<string, string>
+export type SelectionErrors = Record<string, string>;
 
-export type QueryResult = [SelectedWebData, SelectionErrors]
+export type QueryResult = [SelectedWebData, SelectionErrors];
 /**
  * Uses browser automation to open up a website and perform data queries with css
  */
 export default class WebsiteInspector {
   private browser?: puppeteer.Browser;
 
-  constructor() { }
-
+  constructor() {}
 
   private _mapToObjectOrUndefined(map: Map<any, any>) {
-    return map.size === 0 ? undefined : Object.fromEntries(map)
-
+    return map.size === 0 ? undefined : Object.fromEntries(map);
   }
   /**
-   * 
+   *
    * @param url the website URL
    * @param selectors the selectors with which to extract data into the resulting object
    * @returns The data selected from the given website and mapped to object properties
@@ -53,7 +51,7 @@ export default class WebsiteInspector {
 
       const selectedData = new Map();
 
-      const errors = new Map()
+      const errors = new Map();
 
       for (const textSelector of selectors) {
         for (const [key, selector] of Object.entries(textSelector)) {
@@ -62,22 +60,26 @@ export default class WebsiteInspector {
             let elementText = await page.$eval(
               selector,
               (elem) => (elem as HTMLElement).innerText
-            )
+            );
             elementText = elementText.trim();
 
             selectedData.set(key, elementText);
           } catch (error: any) {
+            // handle selection errors
             console.error(
               `Failed to get element innerText. Selector: ${selector}`,
               error
             );
             selectedData.set(key, undefined);
-            errors.set(key, error.message)
+            errors.set(key, error.message);
           }
         }
       }
 
-      return [Object.fromEntries(selectedData), this._mapToObjectOrUndefined(errors)];
+      return [
+        Object.fromEntries(selectedData),
+        this._mapToObjectOrUndefined(errors),
+      ];
     } catch (err) {
       console.error(err);
       throw err;
