6608ea77513d539ad1ed167cbae4bc00e73539bf
Boštjan Kamnik <bkamnik1995@gmail.com>
2023-04-12 10:11:13 +0200

    fix matcher condition

--- src/dom/boardGameGeekReader.spec.ts
+++ src/dom/boardGameGeekReader.spec.ts
@@ -3,19 +3,20 @@ import BoardGameGeekReader, {
 } from "./boardGameGeekReader";
 
 describe("boardGameGeekReader", () => {
+  let boardGGReader: BoardGameGeekReader;
+  beforeEach(() => {
+    boardGGReader = new BoardGameGeekReader("https://boardgamegeek.com");
+  });
+
   describe("getResourceForGameId", () => {
     it("should return an object with id and title", () => {
-      const boardGGReader = new BoardGameGeekReader(
-        "https://boardgamegeek.com"
-      );
       const resourceId = "321608";
       return expect(
         boardGGReader.getResourceForGameId(resourceId)
-      ).resolves.toMatchObject<BoardGameGeekResource>({
-        boardGameGeekId: resourceId,
+      ).resolves.toMatchObject<Partial<BoardGameGeekResource>>({
+        boardGameGeekId: "321608",
         title: "Hegemony: Lead Your Class to Victory",
-        timestamp: "",
       });
-    });
+    }, 10000);
   });
 });
